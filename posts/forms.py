b286a800109688af18f4131630610a7772da2e98
dyachkov-pavel <pahanthebest12345@mail.ru>
2020-09-20 18:14:55 +0300

    new_post

---
+++ posts/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from django.forms import ModelForm
+from posts.models import Post
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ("group", "text")
